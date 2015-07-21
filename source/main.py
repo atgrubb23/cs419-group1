79101ad22ee7e1a4b5bf96cae32acfd59a5e25cc
atgrubb23 <grubba@onid.oregonstate.edu>
2015-07-21 17:02:49 -0600

    hardcoded connection to database

--- source/main.py
+++ source/main.py
@@ -1,3 +1,4 @@
+import MySQLdb
 import curses
 from curses import wrapper
 
@@ -5,7 +6,7 @@ def main(stdscr):
 
 	stdscr = curses.initscr()
 	curses.echo()
-	#stdscr.border(0)
+	stdscr.border(0)
 
 	stdscr.addstr(4, 34, "[INSERT TITLE]", curses.A_STANDOUT)
 
@@ -17,7 +18,6 @@ def main(stdscr):
 
 	win.border(0)
 	
-
 	win.addstr(0, 0, "ENTER LOGIN CREDENTIALS")
 	win.addstr(3, 8, "Username: ")
 	win.addstr(5, 8, "Password: ")
@@ -25,13 +25,19 @@ def main(stdscr):
 	stdscr.refresh()
 	win.refresh()
 
-	un_input = win.getstr()
+	username_db = win.getstr()
 	win.move(5, 18)
 	win.refresh()
-	pw_input = win.getstr()
-	
-	stdscr.addstr(20, 30, "omg i'm blinking", curses.A_BLINK)
+	password_db = win.getstr()
+	hostname_db = "76.103.139.98"
 
+	db = MySQLdb.connect(host=hostname_db, user="user1", passwd="password1", db="cs419db")
+	if db.cursor():
+		stdscr.addstr(20, 30, "Connected!")
+	else:
+		stdscr.addstr(20, 30, "Connection failed.")
+	
+	stdscr.addstr(21, 30, "", curses.A_BLINK)
 	
 	stdscr.getch()
 
