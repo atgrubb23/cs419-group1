45b28eb47962ed0dfbf768c47a2a82718b50d233
hsuja <hsuja@onid.oregonstate.edu>
2015-07-22 15:10:57 -0700

    Update main.py 7/22/15
    Updated main.py with initial pages for database overview and table overview. Currently, the database overview lists the names of all databases, and the table overview lists the table names of the selected database. Note that selecting a database with too many tables (namely information_schema) will cause an error in curses as pagination has not been factored in.

--- source/main.py
+++ source/main.py
@@ -2,6 +2,65 @@ import MySQLdb
 import curses
 from curses import wrapper
 
+def db_overview(stdscr, db):
+
+     cursor = db.cursor()
+     cursor.execute("SHOW DATABASES")
+     data = cursor.fetchall()
+
+     stdscr.clear()
+     stdscr.border(0)
+     stdscr.addstr(4, 34, "DATABASES OVERVIEW", curses.A_STANDOUT)
+     
+     db_list = []
+     i = 0
+     for x in data:
+	  db_list.append(x[0])
+     
+     for name in db_list:
+	  stdscr.addstr(i + 6, 34, str(i + 1) + " " + name)
+	  i += 1
+
+     stdscr.addstr(i + 6, 10, "Select a database by inputting its number and hit ENTER")
+     stdscr.refresh()
+
+     list_idx = int(stdscr.getstr()) - 1
+     stdscr.addstr(i + 7, 10, str(list_idx))
+     db_name = db_list[list_idx]
+
+     table_overview(stdscr, db, db_name)
+     
+
+
+def table_overview(stdscr, db, db_name):
+
+
+     cursor = db.cursor()
+     sql = "USE " + db_name
+     cursor.execute(sql)
+     cursor.execute("SHOW TABLES")
+     data = cursor.fetchall()
+
+     stdscr.clear()
+     stdscr.border(0)
+     stdscr.addstr(4, 34, "TABLES IN " + db_name, curses.A_STANDOUT)
+
+     table_list = []
+     i = 0
+     for x in data:
+	  table_list.append(x[0])
+     for name in table_list:
+	  stdscr.addstr(i + 6, 34, str(i + 1) + " " + name)
+	  i += 1
+     
+     stdscr.refresh()
+
+     #stdscr.getch()
+     #curses.endwin()
+     
+
+
+
 def main(stdscr):
 
 	stdscr = curses.initscr()
@@ -31,14 +90,21 @@ def main(stdscr):
 	password_db = win.getstr()
 	hostname_db = "76.103.139.98"
 
-	db = MySQLdb.connect(host=hostname_db, user="user1", passwd="password1", db="cs419db")
+	#db = MySQLdb.connect(host=hostname_db, user=username_db, passwd=password_db, db="cs419db")
+	db = MySQLdb.connect(host=hostname_db, user=username_db, passwd=password_db)
 	if db.cursor():
 		stdscr.addstr(20, 30, "Connected!")
+		db_overview(stdscr, db)
+		#table_overview(stdscr, db)
+
+	#incorrect credentials produce ugly traceback. does not fail gracefully.
 	else:
 		stdscr.addstr(20, 30, "Connection failed.")
 	
-	stdscr.addstr(21, 30, "", curses.A_BLINK)
+	#stdscr.addstr(21, 30, "", curses.A_BLINK)
 	
+	db.close()
+
 	stdscr.getch()
 
 	curses.endwin()
